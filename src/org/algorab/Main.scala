82ae5821b67273d32aeee47f5b4ff89ba899c630
Raphaël Fromentin <rafbodaha@gmail.com>
2025-11-06 10:44:13 +0100

    test: Add golden case for polymorphic HOF

--- src/org/algorab/Main.scala
+++ src/org/algorab/Main.scala
@@ -8,13 +8,15 @@ import scala.util.Using
 
 object Main extends KyoApp:
 
-  /* 
-
-   */
-
   run:
     // val code = Source.fromFile("test/resources/golden/good/core019.algo").mkString
-    val code = """mut val x = 5
-                 |x = 6""".stripMargin
+    val code = """def id[A](x: A): A = x
+                 |val f = id
+                 |val g = id[String]
+                 |f("hey")
+                 |f(5)
+                 |-- f[String](5)
+                 |g("hey")
+                 |-- g(5)""".stripMargin
                  
     Console.printLine(compile(code))
