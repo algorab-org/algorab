326de03101121493e39d11896e6e5a16baef4f97
Raphaël Fromentin <rafbodaha@gmail.com>
2025-11-27 13:11:53 +0100

    test: Add fizz-buzz golden test

--- src/org/algorab/Main.scala
+++ src/org/algorab/Main.scala
@@ -19,14 +19,14 @@ object Main extends KyoApp:
 
   run:
     direct:
-      // val code = Source.fromFile("test/resources/golden/good/core019.algo").mkString
-      val code = """val x = 5
-                   |def f(y: Int): Int =
-                   |  def g(): Int =
-                   |    val z = 5
-                   |    x + y + z
-                   |  g()""".stripMargin
+      val code = Source.fromFile("test/resources/golden/good/fizzbuzz.algo").mkString
+      // val code = """val x = 5
+      //              |def f(y: Int): Int =
+      //              |  def g(): Int =
+      //              |    val z = 5
+      //              |    x + y + z
+      //              |  g()""".stripMargin
                   
-      Console.printLine(compile(code).map(_.zipWithIndex.map((instr, i) => s"$i: $instr").mkString("\n"))).now
+      // Console.printLine(compile(code).map(_.zipWithIndex.map((instr, i) => s"$i: $instr").mkString("\n"))).now
       // Console.printLine("======================").now
-      // runCode(code).map(Console.printLine).now
+      runCode(code).map(Console.printLine).now
